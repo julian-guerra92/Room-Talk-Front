400fe38973392bf659d303c749b493a32ee25ea2
Julian Rodriguez <Julian Rodrigez>
2024-05-22 12:51:15 -0500

    Se ajusta URL de servicio de back

--- src/api/room-talk-api.ts
+++ src/api/room-talk-api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const roomTalkApi = axios.create({
-   baseURL: 'http://localhost:8080/api',
-   // baseURL: 'https://5j4hxxhk-8080.use2.devtunnels.ms/api',
+   baseURL: 'https://room-talk-api.azurewebsites.net/api',
 });
 
 export default roomTalkApi;
